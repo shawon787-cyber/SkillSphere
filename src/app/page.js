36d7c5f5e58a1121a923b404485a23076b48de82
Md Shawon <shawonmohammad787@gmail.com>
2026-05-01 14:57:15 +0600

    json upddate

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Banner";
+import TopSkills from "@/components/TopSkills";
 
 
 export default function Home() {
   return (
     <div className="">
       <Banner></Banner>
+      <TopSkills></TopSkills>
     </div>
   );
 }
---
+++ src/components/TopSkills.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopSkills = () => {
+    return (
+        <div className='container mx-auto px-4 mt-16'>
+            top
+        </div>
+    );
+};
+
+export default TopSkills;
