ef1063e4f9e967fc2d29a92337c3b291065ce670
Md Shawon <shawonmohammad787@gmail.com>
2026-05-03 23:25:40 +0600

    error facing

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,19 @@
-
 import Banner from "@/components/Banner";
 import Learn from "@/components/Learn";
 import StartLearning from "@/components/StartLearning";
 import TopInstructor from "@/components/TopInstructor";
 import TopSkills from "@/components/TopSkills";
 import TrendingCourse from "@/components/TrendingCourse";
-import ClientWrapper from "@/components/ClientWrapper";
 
 export default function Home() {
   return (
-    <ClientWrapper>
+    <>
       <Banner />
       <TopSkills />
       <Learn />
       <TopInstructor />
       <TrendingCourse />
       <StartLearning />
-    </ClientWrapper>
+    </>
   );
 }
